66d03d4237179eec122bb554053e349c58b960b1
thalitadimas <90683706+thalitadimas@users.noreply.github.com>
2021-10-06 19:55:47 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,25 @@
+<section>
+ 	<h1><?php echo $judul ?></h1>
+	<h4>Nama</h4>
+ 	<ul type="disc">
+ 		<li>Nama Depan : Thalita </li>
+ 		<li>Nama Belakang : Dimastya</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ 		<li> Jalan Haji Mencong RT. 2 RW. 13 No. 7</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Tangerang</li>
+ </ul>
+
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ 	<li>Bulutangkis</li>
+ 	<li>Renang</li>
+
+ </ul>
+</section>
